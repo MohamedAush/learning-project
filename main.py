c2e400a6346442dc281d284df6775645bcff950d
CC63 <CC63@TRADENET.COM.MV>
2026-01-22 11:52:23 +0500

    add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+
+firstName = input("enter your first name:")
+lastName = input("Enter your last name:")
+country = input("Enter your country:")
+age = input("Enter your age:")
+
+print("first name: ", firstName)
+print("last name:", lastName)
+print("country:", country)
+print("I am: ", age)
+
+
